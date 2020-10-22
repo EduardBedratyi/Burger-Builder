8f462f3b764c18d3efc8217c89deeb06fc276b55
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2020-10-22 03:45:26 +0300

    feature/BB-16 created OrderSummary component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,8 +2,8 @@ import React from 'react';
 import classes from './Button.css';
 
 const Button = (props) => (
-<buttton className={[classes.Button, classes[props.btnType]].join(' ')}
- onClick={props.clicked}>{props.children}</buttton>
+<button className={[classes.Button, classes[props.btnType]].join(' ')}
+ onClick={props.clicked}>{props.children}</button>
 );
 
 export { Button };
