cab3a01142d0b6f0fb61f153067e590185252e82
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2020-10-25 20:17:58 +0200

    feature/BB-20 created Toolbar component

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,11 +1,11 @@
-import { link } from 'fs-extra';
 import React from 'react';
-import classes from './NavigationItems';
+import classes from './NavigationItems.css';
 import { NavigationItem } from './NavigationItem/NavigationItem';
 
 const NavigationItems = () => (
     <ul className={classes.NavigationItems}>
-        <NavigationItem />
+        <NavigationItem link="/" active>Burger Builder</NavigationItem>
+        <NavigationItem link="/">Checkout</NavigationItem>
     </ul>
 );
 
