0a846cb89a0de62cd8cea240d3d292868d4ad117
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2020-10-27 00:24:54 +0200

    feature/BB-27 added styles to DrawerToggle component

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './DrawerToggle.css'
 
 const DrawerToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div>
+    <div onClick={props.clicked} className={classes.DrawerToggle}>
+        <diV></diV>
+        <div></div>
+        <div></div>
+   </div>
 );
 
 export { DrawerToggle };
