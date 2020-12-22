4ef4dc4124f75b5e65f2b611e8a2b338e5fa3ffc
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2020-12-22 02:18:27 +0200

    feature/BB-39 created Checkout Component, styled CheckoutSummary component

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -4,11 +4,11 @@ import { Burger } from '../../Burger/Burger';
 import { Button } from '../../UI/Button/Button';
 import classes from './CheckoutSummary.css';
 
-const checkoutSummary = (props) => {
+const CheckoutSummary = (props) => {
   return (
     <div className={classes.CheckoutSummary}>
       <h1>Everything is good!</h1>
-      <div style={{ width: '300px', heght: '300px', margin: 'auto' }}>
+      <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
       <Button btnType='Danger' clicked>
@@ -21,4 +21,4 @@ const checkoutSummary = (props) => {
   );
 };
 
-export { checkoutSummary };
+export { CheckoutSummary };
