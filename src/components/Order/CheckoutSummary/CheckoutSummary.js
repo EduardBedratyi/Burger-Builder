388ec902d6b111e42918590959c8b12ba849cdb8
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2020-12-23 02:23:35 +0200

    feature/BB-44  created checkoutCancelled and continued Handlers

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { isCore } from 'resolve';
-import Burger from '../../Burger/Burger';
+import { Burger } from '../../Burger/Burger';
 import { Button } from '../../UI/Button/Button';
 import classes from './CheckoutSummary.css';
 
@@ -11,10 +11,10 @@ const CheckoutSummary = (props) => {
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType='Danger' clicked>
+      <Button btnType='Danger' clicked={props.checkoutCancelled}>
         CANCEL
       </Button>
-      <Button btnType='Success' clicked>
+      <Button btnType='Success' clicked={props.checkoutContinued}>
         CONTINUE
       </Button>
     </div>
