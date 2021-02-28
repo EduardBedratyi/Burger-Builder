7f64f26396d78016642d162e9a1588a56537ad8b
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2021-02-28 18:00:32 +0200

    feature/BB-65-Checkout updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Layout } from '../src/hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurderBuilder/BurgerBuilder';
-import { Checkout } from './containers/Checkout/Checkout';
+import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 
