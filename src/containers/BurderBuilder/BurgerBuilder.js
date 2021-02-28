0a01977161071f97e40b523f491f22d218967b18
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2021-02-28 15:19:57 +0200

    feature/BB-64-BurgerBuilder-purchasable updated

--- src/containers/BurderBuilder/BurgerBuilder.js
+++ src/containers/BurderBuilder/BurgerBuilder.js
@@ -15,8 +15,10 @@ class BurgerBuilder extends Component {
   //     super(props);
   //     this.state = {...}
   // }
+
+  //these state properties relevant fo UI state only.
+  //(display modal, error message) no need to manage through global redux store
   state = {
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -42,7 +44,7 @@ class BurgerBuilder extends Component {
       .reduce((sum, el) => {
         return sum + el;
       }, 0);
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0;
   }
 
   purchaseHandler = () => {
@@ -94,7 +96,7 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            purchasable={this.state.purchasable}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
             price={this.props.price}
           />
