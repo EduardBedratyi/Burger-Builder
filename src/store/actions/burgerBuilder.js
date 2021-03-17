62e0ec8022c85bca5f8b12a44f716fb2e7f605ea
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2021-03-18 00:34:32 +0200

    feature/BB-72 Initializing Ingredients in the BurgerBuilder

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,17 +1,50 @@
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
 
 const addIngredient = (name) => {
   return {
     type: actionTypes.ADD_INGREDIENT,
-    ingridientName: name,
+    ingredientName: name,
   };
 };
 
 const removeIngredient = (name) => {
   return {
     type: actionTypes.REMOVE_INGREDIENT,
-    ingridientName: name,
+    ingredientName: name,
   };
 };
 
-export { addIngredient, removeIngredient };
+const setIngredients = (ingredients) => {
+  return {
+    type: actionTypes.SET_INGREDIENTS,
+    ingredients: ingredients,
+  };
+};
+
+const fetchIngredientsFailed = () => {
+  return {
+    type: actionTypes.FETCH_INGREDIENTS_FAILED,
+  };
+};
+
+const initIngredients = () => {
+  return (dispatch) => {
+    axios
+      .get('https://burger-project-8c2a2.firebaseio.com/ingredients.json')
+      .then((response) => {
+        dispatch(setIngredients(response.data));
+      })
+      .catch((error) => {
+        dispatch(fetchIngredientsFailed());
+      });
+  };
+};
+
+export {
+  addIngredient,
+  removeIngredient,
+  setIngredients,
+  fetchIngredientsFailed,
+  initIngredients,
+};
