6095b68c10844bd3c752a36e2c3d9e66e6ccee19
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2021-04-11 23:43:11 +0300

    feature/BB-87 implemented Auth validity

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -37,6 +37,51 @@ class Auth extends Component {
     },
   };
 
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+
+    return isValid;
+  }
+
+  inputChangeHandler = (event, controlName) => {
+    const updatedControls = {
+      //this way I can overwrite some of the properties, e.g value and valid
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      },
+    };
+    this.setState({ controls: updatedControls });
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.controls) {
