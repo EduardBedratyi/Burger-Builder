147471d8afe24feb136bae020c098eeb107fa6e5
Eduard Bedratyi <eduard.bedratyi@nearshorepartner.ch>
2021-04-14 01:53:43 +0300

    feature/BB-89 created SignUp action creator

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 const authStart = () => {
@@ -24,6 +25,25 @@ const auth = (email, password) => {
   //here dispatch is in argument thanks to "thunk"
   return (dispatch) => {
     dispatch(authStart());
+    //https://firebase.google.com/docs/reference/rest/auth?authuser=0#section-create-email-password
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    };
+    axios
+      .post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCGvCA6Y_jcsWi7GrW1hosbV6ugT0eWEwM',
+        authData
+      )
+      .then((response) => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
   };
 };
 
