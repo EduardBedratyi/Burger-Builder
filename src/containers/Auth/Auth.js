cbf5a0a03d9c9fa34df2e8bd26cfce3ce39abb32
Eduard_Bedratyi <eduard.bedratyi@jetsoftpro.com>
2022-01-26 21:52:29 +0200

    BB-106: refactor Auth by using updateObject utility function

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import { Button } from "../../components/UI/Button/Button";
 import { Spinner } from "../../components/UI/Spinner/Spinner";
 import classes from "./Auth.css";
 import * as actions from "../../store/actions/index";
+import { updateObject } from "../../shared/utility";
 
 class Auth extends Component {
   state = {
@@ -91,6 +92,16 @@ class Auth extends Component {
         touched: true,
       },
     };
+    const updatedControls = updateObject(this.state.controls, {
+      [controlName]: updateObject(this.state.controls[controlName], {
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true,
+      }),
+    });
     this.setState({ controls: updatedControls });
   };
 
