fec61df838b02d58fa44cfb7cc98f4beb4f14f63
Eduard Bedratyi <0675060050@mail.ru>
2024-01-06 17:37:31 -0500

    BB-113: testing three elements instead two

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -11,4 +11,9 @@ describe('<NavigationItems />', () => {
         const wrapper = shallow(<NavigationItems />);
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
     });
+
+    it('should render three <NavigationItem /> elements if authenticated', () => {
+        const wrapper = shallow(<NavigationItems isAuthenticatedUser />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
 });
