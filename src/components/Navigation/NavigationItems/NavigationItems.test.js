677672609c100ada10a48cae6c57f540015de874
Eduard Bedratyi <0675060050@mail.ru>
2024-08-11 20:07:25 -0400

    BB-116: Observe results of beforeEach and it methods

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -7,10 +7,12 @@ import { NavigationItem } from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+    //Results of describe is in the Test Suits
     let wrapper;
     beforeEach(() => {
         wrapper = shallow(<NavigationItems />);
     });
+    //Results of it are in the Tests
 
     it('should render two <NavigationItem /> elements if not authenticated', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(2);
